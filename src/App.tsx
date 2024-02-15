e3673b30fbd85efa8a02fb067c6e3e6c7cea7fde
Stephen <stesay@udel.edu>
2024-02-15 18:04:15 -0500

    adding for TA review

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row } from "react-bootstrap";
+import img from "./images/image.png";
 
 function App(): JSX.Element {
     return (
@@ -11,6 +13,29 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <header className="App-header"> A new header?</header>
+            <Container>
+                <Row>
+                    <div className="rectangle" />
+                    <p>Wow a list and a button!</p>
+                    <Button onClick={() => console.log("Hello World!")}>
+                        Log Hello World
+                    </Button>
+                    <ul>
+                        <li>list</li>
+                        <li>with</li>
+                        <li>3 elements</li>
+                    </ul>
+                </Row>
+                <Row>
+                    <div className="rectangle" />
+                    <p>Is that a image?</p>
+                    <img
+                        src={img}
+                        alt="I can't believe that this is an image"
+                    />
+                </Row>
+            </Container>
         </div>
     );
 }
