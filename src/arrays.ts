1a9b2821f877ea2ed82efb752cdd357e8a73b61c
Stephen <stesay@udel.edu>
2024-02-19 12:03:45 -0500

    wrote tripleNumbers()

--- src/arrays.ts
+++ src/arrays.ts
@@ -22,7 +22,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((element: number): number => element * 3);
 }
 
 /**
