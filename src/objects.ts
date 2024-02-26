30f1cd5853a993a1ac59a3326b12ab65d54dd37b
Stephen <stesay@udel.edu>
2024-02-26 15:12:05 -0500

    wrote toMarkdown()

--- src/objects.ts
+++ src/objects.ts
@@ -79,7 +79,14 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let printStr = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        printStr += "\n";
+        for (const option of question.options) {
+            printStr += "- " + option + "\n";
+        }
+    }
+    return printStr;
 }
 
 /**
