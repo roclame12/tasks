d39bc73d769e6428a7cb7062c7f77475c66cf581
Stephen <stesay@udel.edu>
2024-02-27 14:01:02 -0500

    findQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -35,7 +35,15 @@ export function findQuestion(
     questions: Question[],
     id: number
 ): Question | null {
-    return null;
+    const found = questions.find(
+        (question: Question): boolean => question.id === id
+    );
+
+    if (found === undefined) {
+        return null; // why?
+    } else {
+        return found;
+    }
 }
 
 /**
