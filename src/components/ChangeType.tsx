a728aabb93df7b871725c4c79bdcebbc7e8b86c7
Stephen <stesay@udel.edu>
2024-03-04 16:39:20 -0500

    wrote revealAnswer and ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -2,6 +2,27 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
+const TYPE_UPDATER: Record<QuestionType, QuestionType> = {
+    multiple_choice_question: "short_answer_question",
+    short_answer_question: "multiple_choice_question"
+};
+
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function changeQuestionType(): void {
+        const newType = TYPE_UPDATER[questionType];
+        setQuestionType(newType);
+    }
+
+    return (
+        <div>
+            <Button onClick={changeQuestionType}>Change Type</Button>
+            {questionType === "short_answer_question"
+                ? "Short Answer"
+                : "Multiple Choice"}
+        </div>
+    );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function flipVisablity(): void {
+        setVisible(!visible);
+    }
+
+    return (
+        <div>
+            <Button onClick={flipVisablity}>Reveal Answer</Button>
+            {visible ? "42" : ""}
+        </div>
+    );
 }
