ab231545ed7228b9ae9aa755fc4c83b98c8070d2
Stephen <stesay@udel.edu>
2024-03-05 17:34:50 -0500

    wrote all forms. Commit before testing

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,43 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
+
+type Holiday =
+    | "Cristmas: 🧑‍🎄"
+    | "Halloween: 👺"
+    | "Independence Day: 💥"
+    | "Labor Day: ☀️"
+    | "Thanksgiving: 🇹🇷";
+
+const BY_DATE: Record<Holiday, Holiday> = {
+    "Independence Day: 💥": "Labor Day: ☀️",
+    "Labor Day: ☀️": "Halloween: 👺",
+    "Halloween: 👺": "Thanksgiving: 🇹🇷",
+    "Thanksgiving: 🇹🇷": "Cristmas: 🧑‍🎄",
+    "Cristmas: 🧑‍🎄": "Labor Day: ☀️"
+};
+const BY_ALPHABET: Record<Holiday, Holiday> = {
+    "Cristmas: 🧑‍🎄": "Halloween: 👺",
+    "Halloween: 👺": "Independence Day: 💥",
+    "Independence Day: 💥": "Labor Day: ☀️",
+    "Labor Day: ☀️": "Thanksgiving: 🇹🇷",
+    "Thanksgiving: 🇹🇷": "Cristmas: 🧑‍🎄"
+};
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("Cristmas: 🧑‍🎄");
+    return (
+        <Col>
+            <h1>{holiday}</h1>
+            <Row>
+                <Col>
+                    <Button onClick={() => setHoliday(BY_DATE[holiday])}>
+                        Advance by Year
+                    </Button>
+                    <Button onClick={() => setHoliday(BY_ALPHABET[holiday])}>
+                        Advance by Alphabet
+                    </Button>
+                </Col>
+            </Row>
+        </Col>
+    );
 }
