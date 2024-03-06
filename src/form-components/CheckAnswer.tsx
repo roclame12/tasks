b5686644686fee62c30f90d0dc2ead15808c808a
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 <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>
2024-03-06 15:47:05 -0500

    wrote CheckAnswers.tsx and GiveAttempts.tsx

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
+
+type InputEvent = React.ChangeEvent<HTMLInputElement>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(event.target.value);
+    }
+
     return (
-        <div>
+        <Col>
             <h3>Check Answer</h3>
-        </div>
+            <Form.Group controlId="formAnswerPrompt">
+                <Form.Label>
+                    Answer: {userAnswer === expectedAnswer ? "✔️" : "❌"}
+                </Form.Label>
+                <Form.Control value={userAnswer} onChange={updateAnswer} />
+            </Form.Group>
+        </Col>
     );
 }
