5dc3598611630675afd119a7eddf5b6a8c0402c8
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 <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>
2024-03-06 20:13:16 -0500

    wrote ChangeColor.tsx

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Row, Col, Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const [chosenColor, setColor] = useState<string>("red");
+    const COLORS: string[] = [
+        "red",
+        "blue",
+        "green",
+        "orange",
+        "purple",
+        "cyan",
+        "magenta",
+        "white"
+    ];
+
     return (
-        <div>
+        <Col>
             <h3>Change Color</h3>
-        </div>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    key={color}
+                    inline
+                    type="radio"
+                    name={color}
+                    onChange={(e) => setColor(e.target.value)}
+                    id={color}
+                    label={
+                        <span style={{ backgroundColor: color }}>{color}</span>
+                    }
+                    value={color}
+                    checked={chosenColor === color}
+                />
+            ))}
+            <Row>
+                <p>
+                    You have chosen{" "}
+                    {
+                        <span style={{ backgroundColor: chosenColor }}>
+                            {chosenColor}
+                        </span>
+                    }
+                </p>
+            </Row>
+        </Col>
     );
 }
